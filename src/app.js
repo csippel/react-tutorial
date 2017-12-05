968ad1d33d2241200383a4ba09e8156574f06b09
csippel <christoph.sippel@glossybox.com>
2017-12-05 16:36:43 +0100

    added style loader for css to webpack config

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import WahlWalApp from './components/WahlWalApp';
+import './styles/style.css';
 
 ReactDOM.render(<WahlWalApp />, document.getElementById('app'));
